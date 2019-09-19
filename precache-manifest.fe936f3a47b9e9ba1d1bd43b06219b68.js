97882896888868a1db94e1be39ebd9b8fcfda83c
unknown <inourbubble2@gmail.com>
2019-09-19 20:17:46 +0900

    Updates

---
+++ precache-manifest.fe936f3a47b9e9ba1d1bd43b06219b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a1b1b7b6abd91ca9e389b26d11470a",
+    "url": "/react-redux-todolist/index.html"
+  },
+  {
+    "revision": "d0ae95bac9fe2821c6d0",
+    "url": "/react-redux-todolist/static/css/main.94632df0.chunk.css"
+  },
+  {
+    "revision": "db52436c434869e3dfa0",
+    "url": "/react-redux-todolist/static/js/2.b9977f1d.chunk.js"
+  },
+  {
+    "revision": "d0ae95bac9fe2821c6d0",
+    "url": "/react-redux-todolist/static/js/main.ef56fda2.chunk.js"
+  },
+  {
+    "revision": "ab91a47dbc96652aaa5d",
+    "url": "/react-redux-todolist/static/js/runtime~main.df17825a.js"
+  }
+]);
